effb3e2277c3266a8cb96caa6746bd2313bd0128
Zhero-Six <vmochoge@gmail.com>
2025-04-02 14:26:18 +0300

    updates

--- test/helpers.js
+++ test/helpers.js
@@ -19,3 +19,98 @@ const src = babelResult.code
 jsdom({
   html, src
 });
+// helpers.js
+
+function myEach(collection, callback) {
+  const values = Array.isArray(collection) ? collection : Object.values(collection);
+  for (let i = 0; i < values.length; i++) {
+    callback(values[i]);
+  }
+  return collection;
+}
+
+function myMap(collection, callback) {
+  const values = Array.isArray(collection) ? collection : Object.values(collection);
+  const result = [];
+  for (let i = 0; i < values.length; i++) {
+    result.push(callback(values[i]));
+  }
+  return result;
+}
+
+function myReduce(collection, callback, acc) {
+  const values = Array.isArray(collection) ? collection : Object.values(collection);
+  let accumulator = acc;
+  let startIndex = 0;
+  
+  if (acc === undefined) {
+    accumulator = values[0];
+    startIndex = 1;
+  }
+  
+  for (let i = startIndex; i < values.length; i++) {
+    accumulator = callback(accumulator, values[i], collection);
+  }
+  return accumulator;
+}
+
+function myFind(collection, predicate) {
+  const values = Array.isArray(collection) ? collection : Object.values(collection);
+  for (let i = 0; i < values.length; i++) {
+    if (predicate(values[i])) {
+      return values[i];
+    }
+  }
+  return undefined;
+}
+
+function myFilter(collection, predicate) {
+  const values = Array.isArray(collection) ? collection : Object.values(collection);
+  const result = [];
+  for (let i = 0; i < values.length; i++) {
+    if (predicate(values[i])) {
+      result.push(values[i]);
+    }
+  }
+  return result;
+}
+
+function mySize(collection) {
+  return Array.isArray(collection) ? collection.length : Object.keys(collection).length;
+}
+
+function myFirst(array, n) {
+  if (n === undefined) {
+    return array[0];
+  }
+  return array.slice(0, n);
+}
+
+function myLast(array, n) {
+  if (n === undefined) {
+    return array[array.length - 1];
+  }
+  return array.slice(-n);
+}
+
+function myKeys(object) {
+  return Object.keys(object);
+}
+
+function myValues(object) {
+  return Object.values(object);
+}
+
+// Export all functions (assuming this is a Node.js module)
+module.exports = {
+  myEach,
+  myMap,
+  myReduce,
+  myFind,
+  myFilter,
+  mySize,
+  myFirst,
+  myLast,
+  myKeys,
+  myValues
+};
